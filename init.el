0bd0c309c15bee0c649875406a1d5e21510629bd
Michael Willis <willismichael@gmail.com>
2014-05-23 21:13:13 -0500

    Finally got around to standardizing my emacs config across machines

---
+++ init.el
@@ -0,0 +1,18 @@
+(set-frame-font "DejaVu Sans Mono-9" nil t)
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+
+;; Auto-install packages, hurray!
+(dolist (p '(better-defaults cider clojure-mode elpy))
+  (when (not (member p package-activated-list))
+    (list-packages)
+    (package-install p)))
+
+(require 'clojure-mode)
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
+(elpy-enable)
